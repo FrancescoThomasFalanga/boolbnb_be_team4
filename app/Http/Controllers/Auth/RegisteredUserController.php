b68e2e08bfff7543ae0fcffa2ead546c07363091
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-12-04 03:02:04 +0100

    fix: fix registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,18 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+
+            'name' => ['max:255', "regex:/^[\pL\s'.]+$/u", 'nullable'],
+            'surname' => ['max:255', "regex:/^[\pL\s'.]+$/u", 'nullable'],
+            'date_of_birth' => ['date', 'nullable'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'surname' => $request->surname,
+            'date_of_birth' => $request->date_of_birth,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
