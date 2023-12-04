74a0d6596406c239b779fd042af6d157befd8486
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-12-04 03:16:50 +0100

    feat: add controllers

--- app/Http/Controllers/Admin/SponsorshipController.php
+++ app/Http/Controllers/Admin/SponsorshipController.php
@@ -3,28 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
+use App\Models\Sponsorship;
 use Illuminate\Http\Request;
+use Braintree\Gateway;
 
 class SponsorshipController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+    protected $gateway;
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function __construct()
     {
-        //
+        $this->gateway = new Gateway([
+            'environment' => env('BRAINTREE_ENV'),
+            'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+            'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+            'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+        ]);
     }
 
     /**
@@ -33,53 +28,44 @@ class SponsorshipController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $slug)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+                // Sponsorship types mapping
+                $sponsorshipTypes = [
+                    '2.99' => ['name' => 'short', 'duration' => 24],
+                    '5.99' => ['name' => 'middle', 'duration' => 72],
+                    '9.99' => ['name' => 'long', 'duration' => 144],
+                ];
+        
+                // Find the apartment with the given slug
+                $apartment = Apartment::where('slug', $slug)->firstOrFail();
+        
+                $amount = $request->amount;
+                $nonce = $request->payment_method_nonce;
+        
+                $result = $this->gateway->transaction()->sale([
+                    'amount' => $amount,
+                    'paymentMethodNonce' => $nonce,
+                    'options' => [
+                        'submitForSettlement' => True
+                    ]
+                ]);
+        
+                if ($result->success) {
+        
+                    if (!array_key_exists($amount, $sponsorshipTypes)) {
+                        return back()->withErrors('Invalid sponsorship type.');
+                    }
+        
+                    $sponsorshipData = $sponsorshipTypes[$amount];
+        
+                    $sponsorship = Sponsorship::where('name', $sponsorshipData['name'])->first();
+        
+                    $apartment->sponsorships()->attach($sponsorship->id, ['start_date' => \Carbon\Carbon::now()]);
+        
+                    return redirect()->route('admin.apartments.show', $apartment->slug)->with('success_message', 'Transaction successful. The sponsorship is now active.');
+                } else {
+                    return back()->withErrors('An error occurred with the message: ' . $result->message);
+                }
     }
 }
